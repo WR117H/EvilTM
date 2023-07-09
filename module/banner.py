f250ba208d29617df899e9ace80e5e6a361b2dc2
Wraith <97615989+WR117H@users.noreply.github.com>
2023-07-10 00:22:38 +0330

    Create banner.py

---
+++ module/banner.py
@@ -0,0 +1,22 @@
+from colorama import Fore
+
+description="""
+EvilTM is a tool to crack NLTM hashes with sam.save 
+and system.save to extract hash of windows password.
+The newest version and documentation is always available at;
+   [*] https://github.com/WR117H/EvilTM
+"""
+
+id="WR117H"
+ascii=f"""
+    ______      _ __________  ___
+   / ____/   __(_) /_  __/  |/  /
+  / __/ | | / / / / / / / /|_/ / 
+ / /___ | |/ / / / / / / /  / /  
+/_____/ |___/_/_/ /_/ /_/  /_/ 
+"""
+
+def ban():
+   print(Fore.LIGHTMAGENTA_EX+ascii+Fore.RESET)
+def disc():
+   print(description)
